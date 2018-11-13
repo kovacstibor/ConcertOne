ba4bee4ee77dded0a8ac48a5eac92359ec6e5510
Tibor Kovács <kovacs.tibor@outlook.hu>
2018-11-13 21:41:03 +0100

    Created empty html index page .

---
+++ ConcertOne.Web/Controllers/ApplicationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConcertOne.Web.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
